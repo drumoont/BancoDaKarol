30b2b964334d1a2905372c49b3a0f475094ccc7e
jesus <karoldrumont@gmail.com>
2017-08-24 16:12:07 -0300

    lesgal

---
+++ AlgumaCoisa/src/main/java/Idade.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class Simples{
+private int idade=10;
+//metodo que altera a idade
+void setAltera(int z){
+idade =z;
+}
+//metodo que mostra a idade
+int getMostra(){
+return idade;
+}
+}
+
